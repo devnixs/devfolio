fc264341be0cd04139995e0b715bb7d55da04758
Raphael <raphael.atallah@xbto.com>
2022-08-09 16:48:13 +0200

    obfuscated email address

--- src/components/section-contact/index.jsx
+++ src/components/section-contact/index.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import Section from '../section';
 
-const SectionContact = ({ experience }) => {
+function rot13(message) {
+  return message.replace(/[a-z]/gi, (letter) =>
+    String.fromCharCode(
+      letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13 : -13)
+    )
+  );
+}
+
+const SectionContact = ({}) => {
   const [emailAddress, setEmailAddress] = React.useState(null);
+  const [revealed, setRevealed] = React.useState(false);
 
   React.useEffect(() => {
-    setEmailAddress('contact [at] thera-engineering.com');
+    setEmailAddress(rot13(atob('cGJhZ25wZ0BndXJlbi1yYXR2YXJyZXZhdC5wYno=')));
   });
 
   return (
-    <Section title="Contact">Feel free to contact me at {emailAddress}</Section>
+    <Section title="Contact">
+      {revealed ? (
+        <>
+          Feel free to contact me at{' '}
+          <a className="email-address" href={'mailto:' + emailAddress}>
+            {emailAddress}
+          </a>
+        </>
+      ) : (
+        <span className="reveal" onClick={() => setRevealed(true)}>
+          Reveal email address
+        </span>
+      )}
+    </Section>
   );
 };
 
